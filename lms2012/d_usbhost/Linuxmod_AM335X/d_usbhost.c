c7b823dd34deb8d083aba17b4950046f2286334f
Sean Chiang <n2i911@gmail.com>
2015-12-16 10:43:11 +0800

    d_usbhost is dummy module for lego ev3

--- lms2012/d_usbhost/Linuxmod_AM335X/d_usbhost.c
+++ lms2012/d_usbhost/Linuxmod_AM335X/d_usbhost.c
@@ -15,15 +15,79 @@ MODULE_AUTHOR("Sean Chiang <n2i911@gmail.com>");
 MODULE_DESCRIPTION(MODULE_NAME);
 MODULE_SUPPORTED_DEVICE(DEVICE1_NAME);
 
+static ssize_t Device1Write(struct file *File, const char *Buffer, size_t Count, loff_t *Data)
+{
+    pr_info("%s, %s\n", MODULE_NAME, __func__);
+
+    return Count;
+}
+
+static ssize_t Device1Read(struct file *File, char *Buffer, size_t Count, loff_t *Offset)
+{
+    int len;
+
+    pr_info("%s, %s\n", MODULE_NAME, __func__);
+
+    len = snprintf(Buffer, Count, "%s\n", DEVICE1_NAME);
+
+    return len;
+}
+
+static const struct file_operations Device1Entries =
+{
+    .owner = THIS_MODULE,
+    .read = Device1Read,
+    .write = Device1Write,
+};
+
+static struct miscdevice Device1 =
+{
+    MISC_DYNAMIC_MINOR,
+    DEVICE1_NAME,
+    &Device1Entries
+};
+
+static int Device1Init(void)
+{
+    int ret = 0;
+
+    pr_info("%s, %s\n", MODULE_NAME, __func__);
+
+    ret = misc_register(&Device1);
+
+    if (ret) {
+        pr_info("%s device register failed\n", DEVICE1_NAME);
+    }
+    else {
+        pr_info("%s device register succes\n", DEVICE1_NAME);
+    }
+
+    return ret;
+}
+
+static void Device1Exit(void)
+{
+    pr_info("%s, %s\n", MODULE_NAME, __func__);
+
+    misc_deregister(&Device1);
+
+    pr_info("%s device unregistered\n", DEVICE1_NAME);
+}
+
 static int ModuleInit(void)
 {
-    pr_devel("%s, %s\n", MODULE_NAME, __func__);
+    pr_info("%s, %s\n", MODULE_NAME, __func__);
+
+    Device1Init();
+
     return 0;
 }
 
 static void ModuleExit(void)
 {
-    pr_devel("%s, %s\n", MODULE_NAME, __func__);
+    pr_info("%s, %s\n", MODULE_NAME, __func__);
+
+    Device1Exit();
 }
 
 module_init(ModuleInit);
